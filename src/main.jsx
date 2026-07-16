afdeb7e23d403b0a0af9f13e26c68296160f8eb0
lagridazineb <lagridazineb@gmail.com>
2026-07-16 22:50:50 +0100

    Refactor main.jsx to use createRoot and StrictMode
    Refactored the main entry point to use createRoot and removed unnecessary providers.

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import { CartProvider } from './context/CartContext'
-import { NotificationProvider } from './context/NotificationContext'
-import { LanguageProvider } from './context/LanguageContext'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'leaflet/dist/leaflet.css'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <LanguageProvider>
-        <AuthProvider>
-          <CartProvider>
-            <NotificationProvider>
-              <App />
-            </NotificationProvider>
-          </CartProvider>
-        </AuthProvider>
-      </LanguageProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
